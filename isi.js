2e5a641013fa39a1c4e2ff407e054c410ed72856
Waditio <tioskyzo@gmail.com>
2024-10-02 14:28:14 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600=black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI<H1>"); 
+document.writeln("<h3>JURUSAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM<H3>");
+document.writeln("<hr width=600 color=black>);
+document.writeln("</center>>");
